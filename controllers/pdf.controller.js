3b58e3419fedbf074037a7bb621d242205887aff
LongND <nguyenduclong12a2nd@gmail.com>
2023-08-18 16:23:29 +0700

    init project

--- controllers/pdf.controller.js
+++ controllers/pdf.controller.js
@@ -5,18 +5,18 @@ const readFile = require('util').promisify(fs.readFile);
 
 exports.print = async (req, res) => {
     const pdfData = {
-        invoiceItems: [
-            { item: 'Website Design', amount: 5000 },
-            { item: 'Hosting (3 months)', amount: 2000 },
-            { item: 'Domain (1 year)', amount: 1000 },
-        ],
-        invoiceData: {
-            invoice_id: 123,
-            transaction_id: 1234567,
-            payment_method: 'Paypal',
-            creation_date: '04-05-1993',
-            total_amount: 141.5,
-        },
+        // invoiceItems: [
+        //     { item: 'Website Design', amount: 5000 },
+        //     { item: 'Hosting (3 months)', amount: 2000 },
+        //     { item: 'Domain (1 year)', amount: 1000 },
+        // ],
+        // invoiceData: {
+        //     invoice_id: 123,
+        //     transaction_id: 1234567,
+        //     payment_method: 'Paypal',
+        //     creation_date: '04-05-1993',
+        //     total_amount: 141.5,
+        // },
         baseUrl: `${req.protocol}://${req.get('host')}` // http://localhost:3000
     }
 
@@ -25,13 +25,17 @@ exports.print = async (req, res) => {
     }
 
     try {
-        const html = await readFile('views/invoice.hbs', 'utf8');  
+        const html = await readFile('views/account-statement.hbs', 'utf8');
         const template = hbs.compile(html);
-        const content = template(pdfData); 
-        
-        const buffer = await htmlPDF.create(content, options);
-        res.attachment('invoice.pdf')
-        res.end(buffer);
+        console.log(template)
+        const renderedHtml = template({});
+
+        res.send(renderedHtml);
+        // const content = template(pdfData);
+
+        // const buffer = await htmlPDF.create(content, options);
+        // res.attachment('invoice.pdf')
+        // res.end(buffer);
     } catch (error) {
         console.log(error);
         res.send('Something went wrong.')
